1bb5940ca1da5d30091704827c62f71548b7f88c
justhoadoan <hoatrongdoan1710@gmail.com>
2025-10-15 00:12:24 +0700

    initialize project

---
+++ Backend/auracontrol/src/main/java/com/example/auracontrol/AuracontrolApplication.java
@@ -0,0 +1,13 @@
+package com.example.auracontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuracontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuracontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/auracontrol/AuracontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auracontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuracontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
