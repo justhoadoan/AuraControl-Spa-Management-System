e12fcac32a542344f812a422c4d1e70f36d664aa
justhoadoan <hoatrongdoan1710@gmail.com>
2025-12-29 11:29:11 +0700

    RND92: update

--- Backend/auracontrol/src/main/java/com/example/auracontrol/admin/service/AdminResourceService.java
+++ Backend/auracontrol/src/main/java/com/example/auracontrol/admin/service/AdminResourceService.java
@@ -4,6 +4,7 @@ import com.example.auracontrol.admin.dto.ResourceDto;
 import com.example.auracontrol.booking.entity.Resource;
 import com.example.auracontrol.booking.repository.ResourceRepository;
 import com.example.auracontrol.exception.DuplicateResourceException;
+import com.example.auracontrol.exception.InvalidRequestException;
 import com.example.auracontrol.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,6 +21,10 @@ public class AdminResourceService {
         return resourceRepository.findAll();
     }
 
+
+    public List<String> getAllResourceTypes() {
+        return resourceRepository.findDistinctTypes();
+    }
     public Resource getResourceById(Integer id) {
         return resourceRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Resource not found with id: " + id));
@@ -27,24 +32,37 @@ public class AdminResourceService {
 
     @Transactional
     public Resource createResource(ResourceDto request) {
+
         if (resourceRepository.existsByName(request.getName())) {
             throw new DuplicateResourceException("Active resource with name '" + request.getName() + "' already exists.");
         }
 
         Resource resource = new Resource();
         resource.setName(request.getName());
+
         resource.setType(request.getType());
+
         resource.setDeleted(false);
 
         return resourceRepository.save(resource);
     }
 
+
+
     @Transactional
     public Resource updateResource(Integer id, ResourceDto request) {
         Resource resource = getResourceById(id);
 
+        // Check trùng tên Resource (khi đổi tên)
         if (resourceRepository.existsByNameAndResourceIdNot(request.getName(), id)) {
             throw new DuplicateResourceException("Resource name '" + request.getName() + "' already exists.");
+        }
+
+        if (!resourceRepository.existsByType(request.getType())) {
+            List<String> availableTypes = resourceRepository.findDistinctTypes();
+            throw new InvalidRequestException(
+                    "Invalid resource type. You can only update to existing types: " + availableTypes
+            );
         }
 
         resource.setName(request.getName());
@@ -52,14 +70,11 @@ public class AdminResourceService {
 
         return resourceRepository.save(resource);
     }
-
     @Transactional
     public void deleteResource(Integer id) {
-
         if (!resourceRepository.existsById(id)) {
             throw new ResourceNotFoundException("Resource not found with id: " + id);
         }
         resourceRepository.deleteById(id);
     }
-
 }
