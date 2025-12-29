019fd0350e4ee3661da9ab8c3769c2bdab94335e
justhoadoan <hoatrongdoan1710@gmail.com>
2025-12-29 16:06:10 +0700

    adjust

--- Backend/auracontrol/src/main/java/com/example/auracontrol/config/SecurityConfig.java
+++ Backend/auracontrol/src/main/java/com/example/auracontrol/config/SecurityConfig.java
@@ -74,7 +74,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "http://localhost"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "http://localhost:80"));
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
